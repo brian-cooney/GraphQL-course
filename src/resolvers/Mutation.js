367bd5ae2d4b1fcfa8f3d1b806bbe40dd3236038
Brian Cooney <bcooney@gmail.com>
2019-09-02 09:20:56 +0100

    Finish update section

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -39,6 +39,30 @@ const Mutation = {
 
     return deletedUsers[0];
   },
+  updateUser(parent, args, { db }, info) {
+    const { id, data } = args;
+    const user = db.users.find(user => user.id === id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+    if (typeof data.email === "string") {
+      const emailTaken = db.users.some(user => user.email === data.email);
+
+      if (emailTaken) {
+        throw new Error("Email taken");
+      }
+      user.email = data.email;
+    }
+
+    if (typeof data.name === "string") {
+      user.name = data.name;
+    }
+    if (typeof data.age !== "undefined") {
+      user.age = data.age;
+    }
+    return user;
+  },
   createPost(parent, args, { db }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
 
@@ -54,6 +78,9 @@ const Mutation = {
     db.posts.push(post);
 
     return post;
+  },
+  updatePost(parent, args, { db }, info) {
+    const { id, data } = args;
   },
   deletePost(parent, args, { db }, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
